f6b685b5a6fc15a77eecba26b45f8a2bf90107eb
KruQQQ <96259271+KruQQQ@users.noreply.github.com>
2024-06-20 21:40:18 +0200

    Update generate_port.sh

--- generate_port.sh
+++ generate_port.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 generate_random_port() {
+    # Lista portów do pominięcia (używane przez system/usługi)
+    used_ports=(22 80 443 8006 3000)  # Dodaj tutaj inne używane porty
+
     while :; do
         PORT=$(shuf -i 1024-65535 -n 1)
-        ss -lntu | grep -q ":$PORT " || break
+        if [[ ! " ${used_ports[@]} " =~ " ${PORT} " ]]; then
+            ss -lntu | grep -q ":$PORT " || break
+        fi
     done
     echo $PORT
 }
